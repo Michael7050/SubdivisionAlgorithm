182d8f22aab8ca40008715a18aab166dfc822dc5
Michael7050 <michael7050@live.com>
2022-09-18 19:25:23 +1200

    Finished more psudocode for brute and exact methods.

--- src/adaAsgnmnt2/Main.java
+++ src/adaAsgnmnt2/Main.java
@@ -1,11 +1,13 @@
 package adaAsgnmnt2;
 
 import java.util.Arrays;
+import java.util.*;
 
-public class Main {
+public class Main
+{
 
-    private static int[][] landValue = {{20, 40, 100, 130, 150, 200},{40, 140, 250, 320, 400, 450}, {100, 250, 350, 420, 450, 500},
-            {130, 320, 420, 500, 600, 700}, {150, 400, 450, 600, 700, 800},{200, 450, 500, 700, 800, 900}};
+    private static int[][] landValue = {{20, 40, 100, 130, 150, 200}, {40, 140, 250, 320, 400, 450}, {100, 250, 350, 420, 450, 500},
+            {130, 320, 420, 500, 600, 700}, {150, 400, 450, 600, 700, 800}, {200, 450, 500, 700, 800, 900}};
 
     //initialise cost for subdividing a metre of land, and number of divides we want,
     //also initialising our bits of land
@@ -14,16 +16,23 @@ public class Main {
     int length = 6;
     private int totalLandValue = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         //test method for checking array
-     //arrayToString(landValue);
-
+        //arrayToString(landValue);
 
 
     }
 
     //Brute Force
+
+    private void bruteForce(int x, int y)
+    {
+        this.totalLandValue = landValue[x][y];
+        bruteForceMethod(x, y);
+        //then traverse data structure and return result with highest data value.
+    }
 
     private void bruteForceMethod(int x, int y)
     {
@@ -55,9 +64,12 @@ public class Main {
                 //reduce cost of split
                 this.totalLandValue = this.totalLandValue - subdivideCost(verticalSplit, x, y);
 
-                //record new values in arraylist(?)
+                //record new values in arraylist(?) TODO
 
                 //run recursive function on the left side, then run it on the right side
+                //have land value be floating??? TODO
+                bruteForceMethod(result1X, result1Y);
+                bruteForceMethod(result2X,result2Y);
             }
         }
 
@@ -77,9 +89,9 @@ public class Main {
                 //reduce cost of split
                 this.totalLandValue = this.totalLandValue - subdivideCost(verticalSplit, x, y);
 
-                //record new values in arraylist(?)
-
-                //run recursive function on the bottom side, then run it on the top side
+                //record new values in arraylist(?) TODO
+                bruteForceMethod(result1X, result1Y);
+                bruteForceMethod(result2X,result2Y);
             }
         }
 
@@ -112,6 +124,96 @@ public class Main {
     //Greedy Approach
 
     //Exact Approach
+    private void exactMethod(int x, int y)
+    {
+        //this is an exact method
+        //goes through each split, returns land value
+        //takes highest value and continues with it.
+
+        boolean vertSplit = true;
+        int temp1X = x;
+        int temp1Y = y;
+        int temp2X = x;
+        int temp2Y = y;
+        int result1X = x;
+        int result2X = x;
+        int result1Y = y;
+        int result2Y = y;
+        int tempLandValue = this.totalLandValue;
+        int currentHighestValue = this.totalLandValue;
+
+        //subtract current chunk of land from value
+        this.totalLandValue = this.totalLandValue - landValue[x][y];
+
+        //go through vertical splits
+        for (int l = 1; l < x; l++)
+        {
+            temp1X = l;
+            temp2X = x-l;
+            temp1Y = y;
+            temp2Y = y;
+            vertSplit = true;
+
+            //for each split, add new value
+            tempLandValue = this.totalLandValue + (landValue[result1X][result1Y] + landValue[result2X][result2Y]);
+            //reduce cost of split
+            tempLandValue = this.totalLandValue - subdivideCost(vertSplit, x, y);
+
+            //if the split is the current best, save current split results to current best
+            if (tempLandValue > currentHighestValue)
+            {
+                currentHighestValue = tempLandValue;
+                result1X = temp1X;
+                result1Y = temp1Y;
+                result2X = temp2X;
+                result2Y = temp1Y;
+            }
+
+        }
+
+        //go through horizontal splits now
+        if (y > 1)
+        {
+            for (int w = 1; w < y; w++)
+            {
+                temp1X = x;
+                temp2X = x;
+                temp1Y = w;
+                temp2Y = y-w;
+                vertSplit = false;
+
+                //for each split, add new value
+                tempLandValue = this.totalLandValue + (landValue[result1X][result1Y] + landValue[result2X][result2Y]);
+                //reduce cost of split
+                tempLandValue = this.totalLandValue - subdivideCost(vertSplit, x, y);
+
+                if (tempLandValue > currentHighestValue)
+                {
+                    currentHighestValue = tempLandValue;
+                    result1X = temp1X;
+                    result1Y = temp1Y;
+                    result2X = temp2X;
+                    result2Y = temp1Y;
+                }
+            }
+
+            if ((result1X == x) && (result1Y == y))
+            {
+                return;
+            }
+
+            //record current splits in data structure TODO
+
+            //then do recursive method with our resultant splits
+            exactMethod(result1X, result1Y);
+            exactMethod(result2X, result2Y);
+        }
+
+
+
+
+        //if result1x = x and result1y = y, then no split, and return
+    }
 
     //print out array for debugging
 
@@ -138,7 +240,4 @@ public class Main {
         }
         return (result * cost);
     }
-
-
-
 }
